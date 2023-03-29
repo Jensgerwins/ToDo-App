2d99c34fd7384dbe825611986efa3b094964f9a3
Jens gerwins <Jens.gerwins@gmail.com>
2023-03-29 22:21:24 +0200

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const textFeld = document.getElementsByClassName("todo-Text");
+const textFeld = document.querySelector(".todo-Text");
 const addTodo = document.querySelector(".add-Todos");
-const deletbtn =document.querySelector(".delete-Todos");
+const deletbtn = document.querySelector(".delete-Todos");
+const listContainer = document.querySelector("#listcontainer");
+
+const state = [];
 
 
-const state ={[]};
 
 
+addTodo.addEventListener("click", addList);
 
-
-addTodo.addEventListener("click" , addList); 
-
-deletbtn.addEventListener("click" , () => {
+deletbtn.addEventListener("click", () => {
     console.log("delete");
 
 });
@@ -18,8 +18,23 @@ deletbtn.addEventListener("click" , () => {
 
 
 function addList() {
- const newEl = document.createElement("ul");
-const nn =  newEl.innerHTML = "lerne";
-  newEl.appendChild(nn);
-     
+    const newEl = document.createElement("ul");
+
+    const newTodo = textFeld.value;
+    state.push(newTodo);
+    state.forEach(item => {
+        const li = document.createElement("li");
+        li.innerText = item;
+        const button = document.createElement("checkbox");
+        button.addEventListener("click", () => {
+
+        })
+        button.innerText = "";
+        li.appendChild(button);
+        newEl.appendChild(li);
+    });
+
+    listContainer.innerText = "";
+    listContainer.appendChild(newEl);
+
 }
